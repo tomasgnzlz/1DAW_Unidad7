35d88c40bf422cdaef91b269470a24514e68ce4f
tgonati327 <tgonati327@g.educaand.es>
2024-03-21 12:42:51 +0100

    a

--- src/main/java/prioridadfifo/ColaPrioridad.java
+++ src/main/java/prioridadfifo/ColaPrioridad.java
@@ -15,31 +15,31 @@ import java.util.List;
  * @param <T>
  */
 public class ColaPrioridad<T> {
-    
+
     private List<ElementoPrioridad> lista;
     private int tamanio;
-    
+
     public ColaPrioridad(int tamanio) {
         this.lista = new ArrayList<>();
         this.tamanio = tamanio;
     }
-    
+
     public List<ElementoPrioridad> getLista() {
         return lista;
     }
-    
+
     public void setLista(List<ElementoPrioridad> lista) {
         this.lista = lista;
     }
-    
+
     public int getTamanio() {
         return tamanio;
     }
-    
+
     public void setTamanio(int tamanio) {
         this.tamanio = tamanio;
     }
-    
+
     @Override
     public String toString() {
         return "ColaPrioridad{" + "lista=" + lista.toString() + ", tamanio=" + tamanio + '}';
@@ -57,7 +57,7 @@ public class ColaPrioridad<T> {
         if (numElementosLista() == tamanio) {
             estado = true;
         }
-        
+
         return estado;
     }
 
@@ -69,21 +69,39 @@ public class ColaPrioridad<T> {
     // Método para sacar elementos
     // elimino el primero ya que estaría ordenada demayora menor
     public T pop() {
+        if (!lista.isEmpty()) {
+            return null;
+        }
         return (T) this.lista.remove(0);
     }
 
     // Método para añadir elementos al final de la lista
     public void push(ElementoPrioridad aux) {
-        if (!pilaLlena()) {
-//            for (ElementoPrioridad elementoPrioridad : lista) {
-//                if (aux.getPrioridad() >= elementoPrioridad.getPrioridad()) {
-//                    this.lista.add(0, aux);
-//                }else{
-//                    this.lista.add( aux);
-//                }
-//            }
-            this.lista.add((ElementoPrioridad) aux);
+//        if (!pilaLlena()) {
+////            for (ElementoPrioridad elementoPrioridad : lista) {
+////                if (aux.getPrioridad() >= elementoPrioridad.getPrioridad()) {
+////                    this.lista.add(0, aux);
+////                }else{
+////                    this.lista.add( aux);
+////                }
+////            }
+//            this.lista.add((ElementoPrioridad) aux);
+//        }
+
+        if (lista.isEmpty()) {          // caso 1
+            this.lista.add(aux);
+        } else if (!pilaLlena()) {      // caso 2
+            this.lista.add(aux);
+            
+            Collections.sort(lista);
+        } else {                        // caso 3
+            if (aux.getPrioridad() > lista.get(lista.size() - 1).getPrioridad()) {
+                lista.remove(lista.size() - 1);
+                this.lista.add(aux);
+                Collections.sort(lista);
+                // lista .eversrd;
+            }
         }
     }
-    
+
 }
